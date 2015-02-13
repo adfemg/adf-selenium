f7b393e35811f5c56c912345a7c9eb6c82b96e9d
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-13 10:56:18 +0100

    removed By instance since it was no longer used now we use JSF component IDs

--- FileExplorer/src/com/redheap/selenium/fragments/SampleFragment1.java
+++ FileExplorer/src/com/redheap/selenium/fragments/SampleFragment1.java
@@ -3,12 +3,10 @@ package com.redheap.selenium.fragments;
 import com.redheap.selenium.component.AdfButton;
 import com.redheap.selenium.component.AdfRegion;
 
-import org.openqa.selenium.By;
-
 
 public class SampleFragment1 extends PageFragment {
 
-    private By switchRegion2Button = By.id("cb1");
+    private final String switchRegion2ButtonId = "cb1";
 
     public SampleFragment1(AdfRegion region) {
         super(region);
@@ -16,7 +14,7 @@ public class SampleFragment1 extends PageFragment {
 
     public SampleFragment2 clickRegion2Button() {
         System.out.println("Clicking 'switch to region 2' button");
-        AdfButton button = findAdfComponent("cb1", AdfButton.class);
+        AdfButton button = findAdfComponent(switchRegion2ButtonId, AdfButton.class);
         button.click();
         waitForPpr();
         return navigatedTo(SampleFragment2.class);
