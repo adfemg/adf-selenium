66b592782009b94cac80175c5aac397ec48597f6
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-13 13:42:48 +0100

    code clean up

--- SeleniumTools/src/com/redheap/selenium/component/AdfRegion.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfRegion.java
@@ -5,26 +5,13 @@ import org.openqa.selenium.WebDriver;
 
 public class AdfRegion extends AdfComponent {
 
-    //private WebElement element;
-
     public AdfRegion(WebDriver driver, String clientid) {
         super(driver, clientid);
-        //this.element = webElement;
     }
 
     @Override
     protected String getExpectedComponentType() {
         return "oracle.adf.RichRegion";
     }
-
-    /**
-     * **for internal use only** Normally AdfComponents don't expose there WebElement as all interaction with the
-     * element has to be through the AdfComponent. This is an exception as PageFragment needs access to the
-     * WebElement for the af:region.
-     * @return RemoteWebElement for the af:region DOM element
-     */
-    //public RemoteWebElement getElement() {
-    //    return (RemoteWebElement) element;
-    //}
 
 }
