df519d31be38adb462006b8729478f0b6134d6e3
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-15 17:26:33 +0100

    include screenshot & source on failure

--- RichClientDemoTest/src/com/redheap/selenium/RichClientDemoTest.java
+++ RichClientDemoTest/src/com/redheap/selenium/RichClientDemoTest.java
@@ -1,16 +1,26 @@
 package com.redheap.selenium;
 
+import com.redheap.selenium.junit.SavePageSourceOnFailure;
+import com.redheap.selenium.junit.ScreenshotOnFailure;
 import com.redheap.selenium.pages.FileExplorer;
 import com.redheap.selenium.pages.RichClientDemo;
+
+import java.io.File;
 
 import java.util.logging.Logger;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 
 public class RichClientDemoTest extends TestCaseBase<RichClientDemo> {
 
-    private static final String HOME_PAGE = "http://jdevadf.oracle.com/adf-richclient-demo/faces/index.jspx";
+    @Rule
+    public TestWatcher screenshotOnFailure = new ScreenshotOnFailure(getDriver(), new File("errors"));
+    @Rule
+    public TestWatcher saveSourceOnFailure = new SavePageSourceOnFailure(getDriver(), new File("errors"));
+
     private static final Logger logger = Logger.getLogger(RichClientDemoTest.class.getName());
 
     public RichClientDemoTest() {
