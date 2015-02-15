4d4675cc69f69a1b596192f16880a316e1e8806c
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-15 17:27:47 +0100

    do not deprecate old Command... components as people might still be using them and they are not deprecated from a testing point of view

--- SeleniumTools/src/com/redheap/selenium/component/AdfCommandButton.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfCommandButton.java
@@ -2,7 +2,6 @@ package com.redheap.selenium.component;
 
 import org.openqa.selenium.WebDriver;
 
-@Deprecated
 public class AdfCommandButton extends AdfButton {
 
     public AdfCommandButton(WebDriver webDriver, String clientid) {
--- SeleniumTools/src/com/redheap/selenium/component/AdfCommandLink.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfCommandLink.java
@@ -2,7 +2,6 @@ package com.redheap.selenium.component;
 
 import org.openqa.selenium.WebDriver;
 
-@Deprecated
 public class AdfCommandLink extends AdfLink {
 
     public AdfCommandLink(WebDriver webDriver, String clientId) {
