ca83f4aadcc38531f5754d36fe43648243137599
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-15 17:25:57 +0100

    added method to disclose tree node by its key path

--- SeleniumTools/src/com/redheap/selenium/component/AdfTree.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfTree.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 
 public class AdfTree extends AdfComponent {
 
+    // see http://jdevadf.oracle.com/adf-richclient-demo/docs/js-subids.html
+    private static final String SUBID_disclosureID = "disclosureID";
+
     public AdfTree(WebDriver driver, String clientid) {
         super(driver, clientid);
     }
@@ -22,6 +25,16 @@ public class AdfTree extends AdfComponent {
     public AdfTree clickNode(By locator) {
         findElement(locator).findElement(By.tagName("a")).click();
         return this;
+    }
+
+    public void discloseNode(int... keypath) {
+        // build full subid, for example [0][1]disclosureId for second node within first top-level node
+        StringBuilder subid = new StringBuilder();
+        for (int i : keypath) {
+            subid.append("[").append(i).append("]");
+        }
+        subid.append(SUBID_disclosureID);
+        findSubIdElement(subid.toString()).click();
     }
 
 }
