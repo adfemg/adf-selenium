fe9ace6578472ae14b6edf2aae0bc8fea956cca2
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-28 11:25:41 +0100

    af:chooseColor skeleton

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfChooseColor.java
@@ -0,0 +1,18 @@
+package com.redheap.selenium.component;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdfChooseColor extends AdfComponent {
+
+    // no subid's known at http://jdevadf.oracle.com/adf-richclient-demo/docs/js-subids.html
+
+    public AdfChooseColor(WebDriver webDriver, String clientid) {
+        super(webDriver, clientid);
+    }
+
+    @Override
+    protected String getExpectedComponentType() {
+        return "oracle.adf.RichChooseColor";
+    }
+
+}
