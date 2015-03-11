e40c5f77c94fff2baae70d49eebb1ff90875abd8
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-03-11 13:22:39 +0100

    log browser make and model

--- SeleniumTools/src/com/redheap/selenium/junit/WebDriverResource.java
+++ SeleniumTools/src/com/redheap/selenium/junit/WebDriverResource.java
@@ -1,11 +1,13 @@
 package com.redheap.selenium.junit;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import oracle.adf.view.rich.automation.selenium.DialogManager;
 
 import org.junit.rules.ExternalResource;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -33,6 +35,11 @@ public class WebDriverResource extends ExternalResource {
         driver = new FirefoxDriver(createProfile());
         if (!Long.valueOf(10).equals(driver.executeScript("return arguments[0]", 10))) {
             throw new WebDriverException("This browser version is not supported due to Selenium bug 8387. See https://code.google.com/p/selenium/issues/detail?id=8387");
+        }
+        if (logger.isLoggable(Level.FINE)) {
+            Capabilities capabilities = driver.getCapabilities();
+            logger.fine("running " + capabilities.getBrowserName() + " version " + capabilities.getVersion() + " on " +
+                        capabilities.getPlatform());
         }
         DialogManager.init(driver, 10000); // timeout of 10 seconds
     }
