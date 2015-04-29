bc71eae92ddd5d857864ef8b41af3c13daad8c57
Niels <ngorter@gmail.com>
2015-04-29 10:28:53 +0200

    Forgot the changed AdfRegion class Simplified JavaScript since original idea of adding labels for Map conversion didn't work out as expected. Split the original domain object into a wrapper with convenience method and logic and a domain object that looks more like a Pojo. Added Javadoc Todo: More convenience methods such as: Ask for maximum severity of all messages / component messages

--- SeleniumTools/src/com/redheap/selenium/component/AdfRegion.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfRegion.java
@@ -1,8 +1,6 @@
 package com.redheap.selenium.component;
 
-import com.redheap.selenium.domain.FacesMessageDomain;
-
-import java.util.Map;
+import com.redheap.selenium.domain.PageMessageWrapper;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -14,13 +12,18 @@ public class AdfRegion extends AdfComponent {
         super(driver, clientid);
     }
 
-    @Override
-    protected Object executeScript(CharSequence script, Object[] args) {
-        return super.executeScript(script, args);
-    }
-
-    public Map getAllMessages() {
-       return FacesMessageDomain.getAllMessages((JavascriptExecutor)getDriver());
+    /**
+     * Convenience method to get all the messages on the page.
+     * <p>
+     * This method was put here mainly so the {@link com.redheap.selenium.page.PageFragment PageFragment} has access
+     * to this method. This is because the method requires a JavascriptExecutor from a driver and PageFragment
+     * doesn't have it while you do want to ask your fragment if there are any messages if you are writing test against
+     * the taskflowtester.
+     * @return A {@link com.redheap.selenium.domain.PageMessageWrapper PageMessageWrapper} object containing
+     * all messages and some convenicence methods
+     */
+    public PageMessageWrapper getAllMessages() {
+        return PageMessageWrapper.getAllMessages((JavascriptExecutor) getDriver());
     }
 
 }
