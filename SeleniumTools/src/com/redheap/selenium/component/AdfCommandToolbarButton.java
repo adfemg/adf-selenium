c9a5ff211fd77d4b614c7c4b1377328688286081
Jeffrey <jeff@jeff.nl>
2015-05-01 13:08:30 +0200

    Added CommandToolbarButton

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfCommandToolbarButton.java
@@ -0,0 +1,11 @@
+package com.redheap.selenium.component;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdfCommandToolbarButton extends AdfButton {
+
+    public AdfCommandToolbarButton(WebDriver webDriver, String clientid) {
+        super(webDriver, clientid);
+    }
+
+}
