ee5d558b368b08718ab315a0d23aa82add1aa730
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-05-20 13:30:32 +0200

    added cancel() to AdfPopup
    Cancels a popup just as if the user would have clicked on the X to close the popup

--- SeleniumTools/src/com/redheap/selenium/component/AdfPopup.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfPopup.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class AdfPopup extends AdfComponent {
 
     private static final String JS_IS_POPUP_VISIBLE = JS_FIND_COMPONENT + "return comp.isPopupVisible();";
+    private static final String JS_CANCEL = JS_FIND_COMPONENT + "comp.cancel();";
 
     public AdfPopup(WebDriver webDriver, String clientid) {
         super(webDriver, clientid);
@@ -12,6 +13,11 @@ public class AdfPopup extends AdfComponent {
 
     public boolean isPopupVisible() {
         return (Boolean) executeScript(JS_IS_POPUP_VISIBLE, getClientId());
+    }
+
+    public void cancel() {
+        executeScript(JS_CANCEL, getClientId());
+        waitForPpr();
     }
 
 }
