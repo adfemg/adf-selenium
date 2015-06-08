ca3da18fa8cae96a0757e35a4d2e32e80344e294
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-06-08 14:16:24 +0200

    merge van AdfInputFile community contribution

--- SeleniumTools/src/com/redheap/selenium/component/AdfInputFile.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfInputFile.java
@@ -6,8 +6,16 @@ import org.openqa.selenium.WebDriver;
 
 public class AdfInputFile extends UixInput {
 
-    public AdfInputFile(WebDriver webDriver, String string) {
-        super(webDriver, string);
+    // subid's at http://jdevadf.oracle.com/adf-richclient-demo/docs/js-subids.html
+    // most are only relevant after partial-submit and update af:inputFile is available
+    private static final String SUBID_content = "content"; // <input type=file>
+    private static final String SUBID_dlg = "dlg"; // <div>
+    private static final String SUBID_label = "label"; // <label>
+    private static final String SUBID_upBtn = "upBtn"; // af:commandButton after partial submit
+    private static final String SUBID_upVal = "upVal"; // <span> after partial submit or null
+
+    public AdfInputFile(WebDriver driver, String clientid) {
+        super(driver, clientid);
     }
 
 }
