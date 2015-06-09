9ba64025c7c54dd8e7f98c960ad47703d8a42e0a
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-06-09 13:48:31 +0200

    addd isSelected() to CommandNavigationItem

--- SeleniumTools/src/com/redheap/selenium/component/AdfCommandNavigationItem.java
+++ SeleniumTools/src/com/redheap/selenium/component/AdfCommandNavigationItem.java
@@ -6,8 +6,14 @@ public class AdfCommandNavigationItem extends AdfComponent {
 
     public static final String COMPONENT_TYPE = "oracle.adf.RichCommandNavigationItem";
 
+    private static final String JS_GET_SELECTED = JS_FIND_COMPONENT + "return comp.getSelected();";
+
     public AdfCommandNavigationItem(WebDriver webDriver, String clientid) {
         super(webDriver, clientid);
+    }
+
+    public boolean isSelected() {
+        return (Boolean) executeScript(JS_GET_SELECTED, getClientId());
     }
 
 }
