ce061b768d86516cb3249ac936039b6f4d492344
VincentHuiberts <tintienonok@gmail.com>
2015-06-11 10:08:06 +0200

    AdfPanelDrawer toegevoegd
    AdfPanelDrawer toegevoegd

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfPanelDrawer.java
@@ -0,0 +1,10 @@
+package com.redheap.selenium.component;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdfPanelDrawer extends AdfComponent {
+
+    public AdfPanelDrawer(WebDriver webDriver, String clientid) {
+        super(webDriver, clientid);
+    }
+}
