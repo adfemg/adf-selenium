10d280301b4e6f0c9937fd2b288bcd4f1a03665c
rimamdi <raoef.imamdi@gmail.com>
2015-06-22 13:58:10 +0200

    added AdfMenu component class with initial unit tests

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfMenu.java
@@ -0,0 +1,11 @@
+package com.redheap.selenium.component;
+ 
+import org.openqa.selenium.WebDriver;
+
+public class AdfMenu extends AdfComponent {
+    
+    public AdfMenu(WebDriver webDriver, String clientId) {
+        super(webDriver, clientId);
+    }
+
+}
