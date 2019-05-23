c6a3e653a480ad8222f789443ff1328eef0882f7
eeremeev <egor.b.eremeev@gmail.com>
2019-05-23 22:54:33 +0300

    AdfDeclarativeComponent class has been introduced

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfDeclarativeComponent.java
@@ -0,0 +1,9 @@
+package com.redheap.selenium.component;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdfDeclarativeComponent extends AdfComponent {
+    public AdfDeclarativeComponent(WebDriver webDriver, String string) {
+        super(webDriver, string);
+    }
+}
