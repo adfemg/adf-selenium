711e69e56af5032e4c0df9f33c05d295b13b439c
eeremeev <egor.b.eremeev@gmail.com>
2019-05-23 22:56:27 +0300

    Classes for some ADF components have beed added

---
+++ SeleniumTools/src/com/redheap/selenium/component/AdfCommandImageLink.java
@@ -0,0 +1,11 @@
+package com.redheap.selenium.component;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdfCommandImageLink extends AdfLink {
+
+    public AdfCommandImageLink(WebDriver webDriver, String clientId) {
+        super(webDriver, clientId);
+    }
+
+}
